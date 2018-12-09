f86d862c94976fc75994464c30e9687cb1c0d66f
songhuizhang <songhui25262@gmail.com>
2018-12-09 14:24:59 +0800

    组件的props

--- src/PostList.js
+++ src/PostList.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import PostItem from './PostItem';
 
+const data = [
+    { title: '大家一起讨论React', author: 'zhangsan', date: '2017-09-01 10:00'},
+    { title: '前端框架，你最喜欢哪儿一个', author: 'lisi', date: '2018-09-01 10:00'},
+    { title: 'web app是主流', author: 'wangwu', date: '2019-09-01 10:00'},
+]
 class PostList extends Component {
     render() {
         return (
             <div>
                 帖子列表：
                 <ul>
-                    <li>大家一起讨论React</li>
-                    <li>前端框架，你最喜欢哪儿一个</li>
-                    <li>web app是主流</li>
+                    {
+                        data.map(item => {
+                            return (
+                                <PostItem title={item.title} author={item.author} date={item.date} />
+                            )
+                        })
+                    }
                 </ul>
             </div>
         )
